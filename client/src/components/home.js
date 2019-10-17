81089bef88c7521f528c67271946bd93c34ffb85
gayankuruppu <example@example.com>
2019-10-17 20:01:32 +0530

    Add home.js

---
+++ client/src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Home page body content</p>
+        </div>
+    );
+};
+
+export default home;
