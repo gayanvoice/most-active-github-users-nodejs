7f909ed4bef9bc2017ccaa75b93fee221f605654
gayankuruppu <example@example.com>
2019-10-29 21:09:00 +0530

    Update Server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = app => {
-    app.use(proxy("/api/*", { target: "http://localhost:4000/" }));
-    //app.use(proxy("/api/*", { target: "http://104.198.135.184:4000/" }));
+    //app.use(proxy("/api/*", { target: "http://localhost:4000/" }));
+    app.use(proxy("/api/*", { target: "http://104.198.135.184:4000/" }));
 };
